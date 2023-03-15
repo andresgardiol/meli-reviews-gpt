25ce8fc2dfe4924650168c418306df738bf9fe85
Fracisco Hanna <franciscohanna92@gmail.com>
2023-03-15 19:52:21 -0300

    fix(options): populate input with current api token

--- options.js
+++ options.js
@@ -1,14 +1,20 @@
-// Get the input field and save button
-var tokenInput = document.getElementById("token");
-var saveButton = document.getElementById("save");
+document.addEventListener("DOMContentLoaded", function () {
+  // Get the input field and save button
+  var tokenInput = document.getElementById("token");
+  var saveButton = document.getElementById("save");
 
-// Add a click event listener to the save button
-saveButton.addEventListener("click", function () {
-  // Get the value of the input field
-  var tokenValue = tokenInput.value;
+  chrome.storage.sync.get("openaiToken", function (data) {
+    tokenInput.value = data.openaiToken;
+  });
 
-  // Save the token in the extension's storage
-  chrome.storage.sync.set({ openaiToken: tokenValue }, function () {
-    alert("Open AI API token saved");
+  // Add a click event listener to the save button
+  saveButton.addEventListener("click", function () {
+    // Get the value of the input field
+    var tokenValue = tokenInput.value;
+
+    // Save the token in the extension's storage
+    chrome.storage.sync.set({ openaiToken: tokenValue }, function () {
+      alert("Open AI API token saved");
+    });
   });
 });
